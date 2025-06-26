bfb9b07ace470fffbf4f99ee626a5825814b8467
ion <ionpascal26@gmail.com>
2025-06-26 17:16:07 +0200

    ADD: LAN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Arena } from './pages/Arena'
+import { Login } from './pages/Login'
 
 function App() {
   const [username, setUsername] = useState('');
@@ -11,30 +12,16 @@ function App() {
 
   if (!inGame) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-2xl mb-4">Enter your username</h1>
-        <input
-          className="border px-2 py-1 mb-4"
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          placeholder="Username"
-        />
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
-          disabled={!username}
-          onClick={() => setInGame(true)}
-        >
-          Start Game
-        </button>
-      </div>
+      <Login
+        username={username}
+        setUsername={setUsername}
+        onStart={() => setInGame(true)}
+      />
     )
   }
 
   return (
-    <>
-      <Arena username={username} />
-    </>
+    <Arena username={username} />
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,37 @@
-import './App.css'
-import { Character } from './components/Character'
-import { Enemy } from './components/Enemy'
+import { useEffect } from "react";
+import {io} from "socket.io-client";
 
-function App() {
- 
+const socket = io("http://localhost:3001");
+
+export function Login({ username, setUsername, onStart }) {
+  const handleStart = () => {
+    socket.emit('start_game', { username });
+    onStart();
+  }
   return (
-    <>
-      <div className='flex justify-between w-[100vw] h-fit'>
-        <Character />
-        <Enemy />
-      </div>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center h-screen"
+      style={{
+        backgroundImage: "url('login_bg2.png')",
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <h1 className="text-2xl mb-4">Enter your username</h1>
+      <input
+        className="border px-2 py-1 mb-4"
+        type="text"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
+        disabled={!username}
+        onClick={handleStart}
+      >
+        Start Game
+      </button>
+    </div>
+  );
 }
-
-export default App
