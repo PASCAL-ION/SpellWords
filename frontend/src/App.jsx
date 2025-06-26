0ba385f739a6f9a85b52ff3347bf3b4d2335a203
paul <paul1.david@epitech.eu>
2025-06-26 15:48:17 +0200

    [add]Typing system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css'
 import { Character } from './components/Character'
 import { Enemy } from './components/Enemy'
+import { Terminal } from './components/Typing/Terminal'
 
 function App() {
  
-  
-
   return (
     <>
       <div className='flex justify-between w-[100vw] h-fit'>
         <Character />
         <Enemy />
       </div>
+      <Terminal nombre_de_lettres={8} />
     </>
   )
 }
