14d31e8bb66a94c5579a96f07fd301ff0e17ce5d
Schiffer440 <arnaud.dugain@orange.fr>
2025-06-26 16:30:52 +0200

    add: linked database tryin to fetch data from db for stats, not working

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -30,6 +33,12 @@ io.on('connection', (socket) => {
 app.get('/', (req, res) => {
   res.send('Serveur Socket.IO opérationnel !');
 });
+
+app.get('/stats', async (req, res) => {
+  const stats = await prisma.stats.findMany();
+  console.log(stats);
+  res.json(stats)
+})
 
 const PORT = 3001;
 server.listen(PORT, () => console.log(`Backend en écoute sur http://localhost:${PORT}`));
